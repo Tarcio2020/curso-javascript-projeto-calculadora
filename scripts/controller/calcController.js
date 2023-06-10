78c1aae2f36ab045cfa4c373953c4f032a79474c
Tarcio Teles <tarcioapps7@gmail.com>
2023-06-10 17:09:16 -0300

    add_botoes

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -6,49 +6,128 @@ Método construtor é aquele que é chamado automaticamente quando existe uma in
 
  ***Refatorar = Melhorar ou refazer o código 
  _________________________________________________________________________________________________________________________________________________________________
+        
+//innerHTML = pegue esse objeto e coloque uma informação lá.
+// Se vc usar o inner e seu html estiver preenchido, primeiro a página será carregada com o HTML depois substituira para o inner
+setInterval === f(x) executada em um intervalo de tempo || em milisegundos.
 
-
-
-
+__________________________________________________________________________________________________________________________________________________________________
 
 ____________________________________________________________________________________________________________________________________________________________*/
 
 class calcController {
     
     constructor() {
-        
-        this._displauCalc = "0"
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateE1 = document.querySelector("#data");
+        this._timeE1 = document.querySelector("#hora");
         this._currentDate
         this.initialize();
+        this.initButtonsEvents();
+
+
     }
 
 
     initialize() {
-        let displayCalcEl = document.querySelector("#display");
-        let dateE1 = document.querySelector("#data");
-        let timeE1 = document.querySelector("#hora");
 
-        displayCalcEl.innerHTML = "5155454564";
-        dateE1.innerHTML = "12/12/1900";
-        timeE1.innerHTML = "12:00";
+
+        this.setDisplayDateTime();
+
+       setInterval(()=>{
+
+            this.setDisplayDateTime();
         
-//innerHTML = pegue esse objeto e coloque uma informação lá.
-// Se vc usar o inner e seu html estiver preenchido, primeiro a página será carregada com o HTML depois substituira para o inner
+        }, 1000);
+
+
+/*
+      let interval = setInterval(()=> {
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+        }, 1000);
+
+        setTimeout(() => {
+            clearInterval(interval);
+
+        }, 10000);
+*/   
     }
 
+
+
+
+addEventListenerAll(element, events, fn) {
+    events.split(' ').forEach(event => {
+        element.addEventListener(event, fn, false);
+    })
+}
+
+
+
+
+
+// querySelector vai fazer uma busca no doc limitada || querySelectorAll vai fazer uma busca geral permitindo que seja feita com mais paramentros.
+    initButtonsEvents () {
+//Aqui colocamos quem queremos que seja monitorado COMANDO: armazene na variavel buttons os elementos do documento com id button tdos filhos da classe e id parts filhos g.
+        
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g"); 
+//For Each vai fazer com que a função percorra cada um dos eventos.
+        buttons.forEach((btn, index) => {
+// addEventListener = evento nativo do JS||aceita apenas um evento por vez.
+            this.addEventListenerAll(btn, "click drag", e=> {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+                btn.style.cursor = "pointer";
+            })
+        } );
+
+    }
+
+    setDisplayDateTime() {
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime() {
+        return this._timeE1.innerHTML;
+    }
+
+    set displayTime(value) {
+        return this._timeE1.innerHTML = value;
+    }
+
+
+    get displayDate() {
+        return this._dateE1.innerHTML;
+    }
+
+    set displayDate(value) {
+        return this._dateE1.innerHTML = value;
+    }
+
+
+
     get displayCalc() {
-        return this._displauCalc;
+        return this._displayCalcEl.innerHTML;
     
     }
 
 
     set displayCalc(valor) {
-        this._displauCalc = valor;
+        this._displayCalcEl.innerHTML = valor;
     }
 
 
     get currentDate() {
-        return this.currentDate;
+        return new Date();
     }
 
     set currentDate(valor) {
